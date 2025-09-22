1aa0cd2d6ddaee77251b7ec73d262c0a5cad876c
fabriciohpmoura <fabricio@lzinformatica.com.br>
2025-09-22 17:14:46 -0300

    Vite config com path correto.

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,17 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
+  // base define o caminho "raiz" dos assets no build
+  // quando publica em https://<usuario>.github.io/news-rocha/
+  // precisa ser '/news-rocha/'
+  // se usar domínio próprio depois, pode voltar pra '/'
+  base: process.env.GITHUB_ACTIONS ? '/news-rocha/' : '/',
 })
